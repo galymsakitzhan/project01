9119753114725ddf6d3ddcca4939a7be47226efe
Galym <galymsakitzhan@gmail.com>
2024-09-16 13:24:44 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,190 @@
+export const carData = {
+  models: [
+    {
+      name: "Zeekr 001",
+      configurations: [
+        {
+          name: "Премиум AWD",
+          price: 22250000,
+          modification: [
+            { title: "590 км", text: "Дальность хода" },
+            { title: "3,8 с", text: "0-100 км/ч" },
+            { title: "400 кВт", text: "Макс. мощность" },
+          ],
+          exterior: [
+            {
+              colorName: "Хрустально-белый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_11/image_s.png?1698438911",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_11/image_27/1698438833_731427781_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_11/image_28/1698438837_979453695_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_11/image_26/1698438828_1309878002_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Электро-синий",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_16/image_s.png?1698439446",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_12/image_29/1698438852_1671378694_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_12/image_30/1698438856_397442189_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_12/image_31/1698438861_237559217_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Техно-серый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_17/image_s.png?1698439455",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_13/image_32/1698438872_952436982_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_13/image_34/1698438881_819340186_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_13/image_33/1698438877_2021101360_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Фантомно-черный",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_14/image_s.png?1698438954",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_14/image_36/1698438899_147482117_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_14/image_37/1698438903_1037729164_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_14/image_35/1698438895_1304414737_b.png",
+              ],
+              price: 0,
+            },
+          ],
+          wheels: [
+            {
+              name: "Стандартные диски",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_10/image_s.png?1698847199",
+              price: 0,
+            },
+          ],
+          interior: [
+            {
+              name: "Угольно-черный и Каменно-серый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_21/image_s.png?1698439951",
+              price: 0,
+              interiorImageUrl: [
+                "./image/black-front.jpg",
+                "./image/black-back.jpg",
+              ],
+            },
+            {
+              name: "Полуночно-синий и Полярно-белый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_22/image_s.png?1698440023",
+              price: 50000,
+              interiorImageUrl: [
+                "./image/white-front.png",
+                "./image/white-back.png",
+              ],
+            },
+          ],
+        },
+        {
+          name: "Флагман AWD",
+          price: 25150000,
+          modification: [
+            { title: "650 км", text: "Дальность хода" },
+            { title: "3,6 с", text: "0-100 км/ч" },
+            { title: "450 кВт", text: "Макс. мощность" },
+          ],
+          exterior: [
+            {
+              colorName: "Хрустально-белый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_11/image_s.png?1698438911",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_15/image_40/1698439362_605994525_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_15/image_39/1698439357_307814052_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_15/image_38/1698439353_267061749_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Электро-синий",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_16/image_s.png?1698439446",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_16/image_41/1698439383_605429762_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_16/image_43/1698439392_1533195003_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_16/image_42/1698439388_920870091_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Техно-серый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_17/image_s.png?1698439455",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_17/image_46/1698439412_65512672_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_17/image_44/1698439403_567339240_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_17/image_45/1698439408_1273930068_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Фантомно-черный",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_14/image_s.png?1698438954",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_18/image_48/1698439427_1543597277_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_18/image_49/1698439431_1471036836_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_18/image_47/1698439423_209876787_b.png",
+              ],
+              price: 0,
+            },
+            {
+              colorName: "Ярко-оранжевый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_19/image_s.png?1698515121",
+              carImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_19/image_154/1699007517_1230498470_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_19/image_153/1699007515_1210269617_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_19/image_152/1699007511_1568373644_b.png",
+              ],
+              price: 0,
+            },
+          ],
+          wheels: [
+            {
+              name: "Флагманские диски",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_20/image_s.png?1698444377",
+              price: 0,
+            },
+          ],
+          interior: [
+            {
+              name: "Угольно-черный и Каменно-серый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_21/image_s.png?1698439951",
+              price: 0,
+              interiorImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_21/image_52/1698439630_879818117_b.jpg",
+                "https://zeekr-configurator.kz/userdata/section/section_21/image_51/1698439625_850695064_b.jpg",
+              ],
+            },
+            {
+              name: "Полуночно-синий и Полярно-белый",
+              imageUrl:
+                "https://zeekr-configurator.kz/userdata/section/section_22/image_s.png?1698440023",
+              price: 50000,
+              interiorImageUrl: [
+                "https://zeekr-configurator.kz/userdata/section/section_22/image_90/1698656571_1842488837_b.png",
+                "https://zeekr-configurator.kz/userdata/section/section_22/image_112/1698742974_1156396147_b.png",
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
---
+++ script.js
@@ -0,0 +1,416 @@
+import { carData } from "./data.js";
+
+let selectedExteriorImages = [];
+let selectedInteriorImages = [];
+
+function displayConfigurations(model) {
+  const equipmentContainer = document.querySelector(".option__car");
+  equipmentContainer.innerHTML = "";
+  model.configurations.forEach((config, index) => {
+    const configDiv = document.createElement("div");
+    configDiv.classList.add("main_conf");
+    configDiv.classList.add("equipment");
+
+    const configName = document.createElement("div");
+    configName.classList.add("equipment__name");
+    configName.textContent = config.name;
+
+    const configPrice = document.createElement("div");
+    configPrice.classList.add("equipment__price");
+    configPrice.textContent = `${config.price}₸`;
+
+    configDiv.appendChild(configName);
+    configDiv.appendChild(configPrice);
+
+    configDiv.addEventListener("click", () => {
+      const previouslySelected = document.querySelector(".equipment.selected");
+      if (previouslySelected) {
+        previouslySelected.classList.remove("selected");
+      }
+      configDiv.classList.add("selected");
+
+      renderCarData(model, config);
+    });
+
+    equipmentContainer.appendChild(configDiv);
+
+    if (index === 0) {
+      configDiv.click();
+    }
+  });
+}
+
+// Инициализация данных при загрузке страницы
+document.addEventListener("DOMContentLoaded", () => {
+  const selectedModel = carData.models[0]; // Выбираем первую модель для отображения
+
+  // Отображаем конфигурации модели как кнопки
+  displayConfigurations(selectedModel);
+
+  // Отображаем данные для первой конфигурации по умолчанию (handled by configDiv.click())
+});
+
+// Функция для расчета общей цены
+function calculateTotalPrice(config) {
+  let totalPrice = config.price; // Начальная цена конфигурации
+
+  // Добавляем цену выбранного цвета
+  const selectedExterior = config.exterior.find((ex) => ex.isSelected);
+  if (selectedExterior) totalPrice += selectedExterior.price;
+
+  // Добавляем цену выбранного колеса
+  const selectedWheel = config.wheels.find((w) => w.isSelected);
+  if (selectedWheel) totalPrice += selectedWheel.price;
+
+  // Добавляем цену выбранного интерьера
+  const selectedInterior = config.interior.find((i) => i.isSelected);
+  if (selectedInterior) totalPrice += selectedInterior.price || 0;
+
+  return totalPrice;
+}
+
+// Функция для отображения данных автомобиля (цвета, колеса, интерьер)
+function renderCarData(model, selectedConfig) {
+  document.querySelector(".names").textContent = model.name;
+
+  displayModifications(selectedConfig);
+  displayExterior(selectedConfig); // Отображаем цвета экстерьера
+  displayWheels(selectedConfig);
+  displayInterior(selectedConfig);
+
+  // Инициализируем выбранные изображения экстерьера по умолчанию
+  if (selectedConfig.exterior.length > 0) {
+    selectedExteriorImages = selectedConfig.exterior[0].carImageUrl;
+  }
+
+  if (selectedConfig.interior.length > 0) {
+    selectedInteriorImages = selectedConfig.interior[0].interiorImageUrl;
+  }
+
+  // Отображаем объединенные изображения в слайдере
+  updateSlider(selectedExteriorImages.concat(selectedInteriorImages));
+
+  // Расчет и отображение общей цены
+  const totalPrice = calculateTotalPrice(selectedConfig);
+  document.querySelector(".total-price").textContent = `${totalPrice}₸`;
+}
+
+// Функция для переключения изображений в слайдере
+function updateSlider(imageUrls) {
+  let currentIndex = 0;
+  const sliderImage = document.querySelector("#slider-image");
+  const thumbnailsContainer = document.querySelector("#thumbnails");
+
+  // Отображаем первое изображение
+  sliderImage.src = imageUrls[currentIndex];
+
+  // Очищаем контейнер миниатюр перед добавлением новых
+  thumbnailsContainer.innerHTML = "";
+
+  // Создаем миниатюры для каждого изображения
+  imageUrls.forEach((url, index) => {
+    const thumbnail = document.createElement("img");
+    thumbnail.src = url;
+    thumbnail.addEventListener("click", () => {
+      currentIndex = index;
+      sliderImage.src = url;
+    });
+    thumbnailsContainer.appendChild(thumbnail);
+  });
+
+  document.querySelector("#prev-image").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    sliderImage.src = imageUrls[currentIndex];
+  });
+
+  document.querySelector("#next-image").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % imageUrls.length;
+    sliderImage.src = imageUrls[currentIndex];
+  });
+}
+
+// Функции для отображения различных частей автомобиля
+function displayModifications(config) {
+  const modificationContainer = document.querySelector(".modification");
+  modificationContainer.innerHTML = ""; // Очищаем контейнер перед добавлением
+  config.modification.forEach((mod) => {
+    const col = document.createElement("div");
+    col.classList.add("col");
+    col.innerHTML = `
+      <div class="title-sm">${mod.title}</div>
+      <div class="text-sm">${mod.text}</div>
+    `;
+    modificationContainer.appendChild(col);
+  });
+}
+
+function displayExterior(config) {
+  const exteriorContainer = document.querySelector(".exterior .car__colors");
+  const nameExterior = document.querySelector(".name__exterior");
+  const priceExterior = document.querySelector(".price__exterior");
+
+  exteriorContainer.innerHTML = ""; // Очищаем контейнер перед добавлением
+
+  config.exterior.forEach((color, index) => {
+    const button = document.createElement("button");
+    button.innerHTML = `<img src="${color.imageUrl}" alt="${color.colorName}" width="85px">`;
+    button.dataset.carImageUrl = JSON.stringify(color.carImageUrl); // Сохраняем массив изображений в атрибуте data
+
+    // Устанавливаем первое имя и цену по умолчанию
+    if (index === 0 && selectedExteriorImages.length === 0) {
+      nameExterior.textContent = color.colorName;
+      priceExterior.textContent = `${color.price}₸`;
+      selectedExteriorImages = color.carImageUrl; // Инициализируем выбранный экстерьер
+      color.isSelected = true; // Отмечаем первый цвет как выбранный
+    } else {
+      color.isSelected = false;
+    }
+
+    button.addEventListener("click", () => {
+      const previousColor = config.exterior.find((c) => c.isSelected);
+      if (previousColor) previousColor.isSelected = false;
+
+      color.isSelected = true; // Отмечаем выбранный цвет
+      selectedExteriorImages = color.carImageUrl; // Обновляем выбранные изображения экстерьера
+
+      nameExterior.textContent = color.colorName;
+      priceExterior.textContent = `${color.price}₸`;
+
+      // Обновляем массив изображений для машины
+      updateSlider(selectedExteriorImages.concat(selectedInteriorImages)); // Объединяем картинки для слайдера
+
+      // Обновляем общую цену
+      const totalPrice = calculateTotalPrice(config);
+      document.querySelector(".total-price").textContent = `${totalPrice}₸`;
+    });
+
+    exteriorContainer.appendChild(button);
+  });
+}
+
+function displayWheels(config) {
+  const wheelsContainer = document.querySelector(".wheels .car__colors");
+  const nameWheels = document.querySelector(".name__wheels");
+  const priceWheels = document.querySelector(".price__wheels");
+
+  wheelsContainer.innerHTML = ""; // Очищаем контейнер перед добавлением
+
+  config.wheels.forEach((wheel, index) => {
+    const button = document.createElement("button");
+    button.classList.add("wheel__button");
+    button.innerHTML = `<img src="${wheel.imageUrl}" alt="${wheel.name}" width="90px">`;
+
+    // Устанавливаем имя и цену первого колеса по умолчанию
+    if (index === 0 && selectedInteriorImages.length === 0) {
+      nameWheels.textContent = wheel.name;
+      priceWheels.textContent = `${wheel.price}₸`;
+      wheel.isSelected = true; // Отмечаем первое колесо как выбранное
+    } else {
+      wheel.isSelected = false;
+    }
+
+    button.addEventListener("click", () => {
+      const previousWheel = config.wheels.find((w) => w.isSelected);
+      if (previousWheel) previousWheel.isSelected = false;
+
+      wheel.isSelected = true; // Отмечаем выбранное колесо
+
+      nameWheels.textContent = wheel.name;
+      priceWheels.textContent = `${wheel.price}₸`; // Обновляем цену колес
+
+      // Обновляем общую цену
+      const totalPrice = calculateTotalPrice(config);
+      document.querySelector(
+        ".total-price"
+      ).textContent = `Итоговая цена: ${totalPrice}₸`;
+    });
+
+    wheelsContainer.appendChild(button);
+  });
+}
+
+function displayInterior(config) {
+  const interiorContainer = document.querySelector(".interior .car__colors");
+  const nameInterior = document.querySelector(".name__interior");
+  const priceInterior = document.querySelector(".price__interior");
+
+  interiorContainer.innerHTML = ""; // Очищаем контейнер перед добавлением
+
+  config.interior.forEach((interior, index) => {
+    const button = document.createElement("button");
+    button.innerHTML = `<img src="${interior.imageUrl}" alt="${interior.name}" width="85px">`;
+
+    // Устанавливаем первое имя и цену по умолчанию
+    if (index === 0 && selectedInteriorImages.length === 0) {
+      nameInterior.textContent = interior.name;
+      priceInterior.textContent = `${interior.price || 0}₸`; // Если цена есть, выводим, иначе 0
+      selectedInteriorImages = interior.interiorImageUrl; // Инициализируем выбранный интерьер
+      interior.isSelected = true; // Отмечаем первый интерьер как выбранный
+    } else {
+      interior.isSelected = false;
+    }
+
+    button.addEventListener("click", () => {
+      const previousInterior = config.interior.find((i) => i.isSelected);
+      if (previousInterior) previousInterior.isSelected = false;
+
+      interior.isSelected = true; // Отмечаем выбранный интерьер
+      selectedInteriorImages = interior.interiorImageUrl; // Обновляем выбранные изображения интерьера
+
+      nameInterior.textContent = interior.name;
+      priceInterior.textContent = `${interior.price || 0}₸`; // Обновляем цену интерьера
+
+      // Обновляем массив изображений интерьера
+      updateSlider(selectedExteriorImages.concat(selectedInteriorImages)); // Объединяем картинки для слайдера
+
+      // Обновляем общую цену
+      const totalPrice = calculateTotalPrice(config);
+      document.querySelector(
+        ".total-price"
+      ).textContent = `Итоговая цена: ${totalPrice}₸`;
+    });
+
+    interiorContainer.appendChild(button);
+  });
+}
+
+// Инициализация данных при загрузке страницы
+document.addEventListener("DOMContentLoaded", () => {
+  const selectedModel = carData.models[0]; // Выбираем первую модель для отображения
+
+  // Отображаем конфигурации модели как кнопки
+  displayConfigurations(selectedModel);
+
+  // Отображаем данные для первой конфигурации по умолчанию
+  renderCarData(selectedModel, selectedModel.configurations[0]);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const request = document.querySelector(".request");
+  const button = document.getElementById("add-section-button");
+
+  // Изначально скрыть section.request и показать section.sidebar
+  request.style.display = "none";
+
+  button.addEventListener("click", () => {
+    // Переключить видимость секций
+    if (request.style.display === "none") {
+      sidebar.style.display = "none";
+      request.style.display = "block";
+    } else {
+      sidebar.style.display = "block";
+      request.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const request = document.querySelector(".request");
+  const button1 = document.getElementById("back-button");
+
+  button1.addEventListener("click", () => {
+    if (request.style.display === "none") {
+      sidebar.style.display = "none";
+      request.style.display = "block";
+    } else {
+      sidebar.style.display = "block";
+      request.style.display = "none";
+    }
+  });
+});
+
+let selectedCar = {};
+let formData = {};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("add-section-button")
+    .addEventListener("click", () => {
+      const selectedModel = document.querySelector(".names").textContent;
+      const selectedConfiguration = document.querySelector(
+        ".equipment.main_conf.selected .equipment__name"
+      ).textContent;
+      const selectedModelPrice =
+        document.querySelector(".equipment__price").textContent;
+      const selectedExteriorColor =
+        document.querySelector(".name__exterior").textContent;
+      const exteriorColorPrice =
+        document.querySelector(".price__exterior").textContent;
+      const selectedWheels =
+        document.querySelector(".name__wheels").textContent;
+      const selectedWheelsPrice =
+        document.querySelector(".price__wheels").textContent;
+      const selectedInterior =
+        document.querySelector(".name__interior").textContent;
+      const selectedInteriorPrice =
+        document.querySelector(".price__interior").textContent;
+      const totalPrice = document.querySelector(".total-price").textContent;
+
+      selectedCar = {
+        model: selectedModel,
+        model_price: selectedModelPrice,
+        configuration: selectedConfiguration,
+        exteriorColor: selectedExteriorColor,
+        exteriorColorPrice: exteriorColorPrice,
+        wheels: selectedWheels,
+        wheels_price: selectedWheelsPrice,
+        interior: selectedInterior,
+        interior_price: selectedInteriorPrice,
+        totalPrice: totalPrice,
+      };
+
+      document.querySelector(".model__").textContent = selectedCar.model;
+      document.querySelector(".model__price").textContent =
+        selectedCar.model_price;
+      document.querySelector(".configuration__").textContent =
+        selectedCar.configuration;
+      document.querySelector(".exteriorColor__").textContent =
+        selectedCar.exteriorColor;
+      document.querySelector(".exteriorColor__price").textContent =
+        selectedCar.exteriorColorPrice;
+      document.querySelector(".wheels__").textContent = selectedCar.wheels;
+      document.querySelector(".wheels__price").textContent =
+        selectedCar.wheels_price;
+      document.querySelector(".interior__").textContent = selectedCar.interior;
+      document.querySelector(".interior__price").textContent =
+        selectedCar.interior_price;
+      document.querySelector(".totalPrice__").textContent =
+        selectedCar.totalPrice;
+    });
+});
+
+document
+  .getElementById("customer-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    formData = {
+      firstName: document.getElementById("first-name").value,
+      lastName: document.getElementById("last-name").value,
+      phone: document.getElementById("phone").value,
+      email: document.getElementById("email").value,
+      dealer: document.getElementById("dealer").value,
+    };
+    document.getElementById("customer-form").reset();
+
+    const getFullBaseData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      phone: formData.phone,
+      email: formData.email,
+      model: selectedCar.model,
+      model_price: selectedCar.model_price,
+      configuration: selectedCar.configuration,
+      exteriorColor: selectedCar.exteriorColor,
+      exteriorColorPrice: selectedCar.exteriorColorPrice,
+      wheels: selectedCar.wheels,
+      wheels_price: selectedCar.wheels_price,
+      interior: selectedCar.interior,
+      interior_price: selectedCar.interior_price,
+      totalPrice: selectedCar.totalPrice,
+    };
+
+    console.log("Full form and car data:", getFullBaseData);
+  });
