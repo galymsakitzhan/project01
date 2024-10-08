218986d2c1a1e0e82f5d4418888c25d3115f9aff
Galym <galymsakitzhan@gmail.com>
2024-10-08 08:12:14 +0500

    update project

--- script.js
+++ script.js
@@ -95,23 +95,53 @@ function displayConfigurations(model) {
   });
 }
 
-function renderCarData(model, selectedConfig) {
-  document.querySelector(".names").textContent = model.name;
+  function renderCarData(model, selectedConfig) {
+    // Отображение имени модели
+    document.querySelector(".names").textContent = model.name;
 
-  displayModifications(selectedConfig);
-  displayExterior(selectedConfig); // Обновляем экстерьер
-  displayWheels(selectedConfig); // Обновляем колеса
-  displayInterior(selectedConfig); // Обновляем интерьер
+    // Обновляем секции модификаций, экстерьера, колес и интерьера
+    displayModifications(selectedConfig);
+    displayExterior(selectedConfig); // Обновляем экстерьер
+    displayWheels(selectedConfig); // Обновляем колеса
+    displayInterior(selectedConfig); // Обновляем интерьер
 
-  // Устанавливаем слайдер с изображениями первого экстерьера и интерьера
-  selectedExteriorImages = selectedConfig.exterior[0].carImageUrl;
-  selectedInteriorImages = selectedConfig.interior[0].interiorImageUrl;
-  updateSlider(selectedExteriorImages.concat(selectedInteriorImages));
+    // Устанавливаем слайдер с изображениями первого экстерьера и интерьера
+    selectedExteriorImages = selectedConfig.exterior[0].carImageUrl;
+    selectedInteriorImages = selectedConfig.interior[0].interiorImageUrl;
+    updateSlider(selectedExteriorImages.concat(selectedInteriorImages));
 
-  const totalPrice = calculateTotalPrice(selectedConfig);
-  document.querySelector(".total-price").textContent = formatPrice(totalPrice);
-  displayPacket(selectedConfig, model.name);
-}
+    // Рассчитываем итоговую цену
+    const totalPrice = calculateTotalPrice(selectedConfig);
+    document.querySelector(".total-price").textContent =
+      formatPrice(totalPrice);
+
+    // Обновляем пакет, если есть
+    displayPacket(selectedConfig, model.name);
+
+    // Добавляем проверку модели для отображения спецификаций
+    const specElement = document.querySelector(".specifications");
+    specElement.innerHTML = ""; // Очищаем содержимое перед добавлением нового
+
+    if (model.name === "Zeekr 001") {
+      // Если выбрана модель Zeekr 001, отображаем спецификацию для нее
+      specElement.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="fill: orange;">
+          <path d="M331-431h37v-83h48q15.725 0 26.362-10.638Q453-535.275 453-551v-48q0-15.725-10.638-26.362Q431.725-636 416-636h-85v205Zm37-120v-48h48v48h-48Zm129 120h84q15 0 26-10.638 11-10.637 11-26.362v-131q0-15.725-11-26.362Q596-636 581-636h-84v205Zm37-37v-131h47v131h-47Zm133 37h37v-83h50v-37h-50v-48h50v-37h-87v205ZM260-200q-24 0-42-18t-18-42v-560q0-24 18-42t42-18h560q24 0 42 18t18 42v560q0 24-18 42t-42 18H260Zm0-60h560v-560H260v560ZM140-80q-24 0-42-18t-18-42v-620h60v620h620v60H140Zm120-740v560-560Z"></path>
+        </svg>
+        <p style="margin: 13px 0; font-size: 18px;">
+          <a style="color: orange;" href="https://zeekr-configurator.kz/userdata/rubrics/rubrics_2/pdf_ru.pdf?1725615272" target="_blank">Спецификация</a>
+        </p>`;
+    } else if (model.name === "Zeekr X") {
+      // Если выбрана модель Zeekr X, отображаем спецификацию для нее
+      specElement.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="fill: orange;">
+          <path d="M331-431h37v-83h48q15.725 0 26.362-10.638Q453-535.275 453-551v-48q0-15.725-10.638-26.362Q431.725-636 416-636h-85v205Zm37-120v-48h48v48h-48Zm129 120h84q15 0 26-10.638 11-10.637 11-26.362v-131q0-15.725-11-26.362Q596-636 581-636h-84v205Zm37-37v-131h47v131h-47Zm133 37h37v-83h50v-37h-50v-48h50v-37h-87v205ZM260-200q-24 0-42-18t-18-42v-560q0-24 18-42t42-18h560q24 0 42 18t18 42v560q0 24-18 42t-42 18H260Zm0-60h560v-560H260v560ZM140-80q-24 0-42-18t-18-42v-620h60v620h620v60H140Zm120-740v560-560Z"></path>
+        </svg>
+        <p style="margin: 13px 0; font-size: 18px;">
+          <a style="color: orange;" href="https://configurator.zeekralmaty.kz/userdata/rubrics/rubrics_7/pdf_ru.pdf?1725615303" target="_blank">Спецификация</a>
+        </p>`;
+    }
+  }
 
 function createPriceElement(config, withTradeIn = true) {
   const configPrice = document.createElement("div");
